830201d6e8384ab532a0995c4efb5c06800d1707
Semyon Tsebulskii <Semyon.Tsebulskii@gmail.com>
2024-12-14 14:06:33 +0100

    Fix "Unmapped target property: "guid""

--- src/main/java/org/joyapi/mapper/PostMapper.java
+++ src/main/java/org/joyapi/mapper/PostMapper.java
@@ -3,12 +3,15 @@ package org.joyapi.mapper;
 import com.source.client.model.PostDTO;
 import org.joyapi.model.Post;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper
 public interface PostMapper {
 
+    @Mapping(target = "guid", ignore = true)
     Post toPost(PostDTO postDTO);
+
     List<Post> toPostList(List<PostDTO> postDTOs);
 }
