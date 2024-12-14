115c27b245e88efa87cc820d09a7407cf66e71da
Semyon Tsebulskii <Semyon.Tsebulskii@gmail.com>
2024-12-14 12:38:34 +0100

    Add db and repo

--- src/main/java/org/joyapi/model/Post.java
+++ src/main/java/org/joyapi/model/Post.java
@@ -3,20 +3,34 @@ package org.joyapi.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.persistence.*;
+
+import java.util.UUID;
 
 @Getter
 @Setter
+@Entity
+@Table
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "guid", nullable = false)
+    private UUID guid;
+
     @JsonProperty("file_url")
+    @Column(name = "fileUrl", nullable = false)
     private String fileUrl;
 
     @JsonProperty("id")
+    @Column(nullable = false)
     private int id;
 
     @JsonProperty("score")
+    @Column(nullable = true)
     private int score;
 
     @JsonProperty("tags")
+    @Column(nullable = false)
     private String tags;
 }
---
+++ src/main/java/org/joyapi/service/PostService.java
@@ -0,0 +1,31 @@
+package org.joyapi.service;
+
+import lombok.AllArgsConstructor;
+import org.joyapi.model.Post;
+import org.joyapi.repos.PostRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Service
+public class PostService {
+    private PostRepository postRepository;
+
+    public Post savePost(Post post) {
+        return postRepository.save(post);
+    }
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    public Post getPostById(UUID guid) {
+        return postRepository.findById(guid).orElse(null);
+    }
+
+    public void deletePost(UUID guid) {
+        postRepository.deleteById(guid);
+    }
+}
